e0cba9013911ae02c598743be6b338541b51fbfb
Marine Van Hees <vanheesmarine@gmail.com>
2024-01-22 11:35:39 +0100

    solving route problem

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,18 +7,11 @@ import Search from "./Pages/Search";
 import Profile from "./Pages/Profile";
 import LoginForm from "./Components/LoginForm/LoginForm";
 import SignInForm from "./Components/SignInForm/SignInForm";
-import TitleH1 from "./Components/Logo/TitleH1";
-import Navbar from "./Components/Navbar/Navbar";
-import Post from "./Components/Post/Post";
-import Posts from "./Components/Posts/Posts";
-import Avatar from "./Components/Profile/Avatar";
-import ProfileCard from "./Components/Profile/ProfileCard";
-import Salutation from "./Components/Salutation/Salutation";
-import TitleH2 from "./Components/Title/TitleH2";
 
 function App() {
   return (
     <BrowserRouter>
+    <Router>
       <Routes>
         <Route index element={<Register />} />
         <Route path="/login" element={<LoginForm />} />
@@ -26,15 +19,8 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/search" element={<Search />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/titleh1" element={<TitleH1 />} />
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/post" element={<Post />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/avatar" element={<Avatar />} />
-        <Route path="/profilecard" element={<ProfileCard />} />
-        <Route path="/salutation" element={<Salutation />} />
-        <Route path="/titileh2" element={<TitleH2 />} />
       </Routes>
+    </Router>
     </BrowserRouter>
   );
 }
